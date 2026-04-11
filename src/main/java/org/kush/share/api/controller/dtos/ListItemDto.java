aedc8ca0f10b4c888cefcc1081ab11577600718a
LOL211 <30016924+LOL211@users.noreply.github.com>
2026-04-11 15:04:25 +0800

    standarize naming

--- src/main/java/org/kush/share/api/controller/dtos/ListItemDto.java
+++ src/main/java/org/kush/share/api/controller/dtos/ListItemDto.java
@@ -3,6 +3,6 @@ package org.kush.share.api.controller.dtos;
 public record ListItemDto(
         String link,
         String description,
-        String itemId
+        String id
 )
 {}
--- src/main/java/org/kush/share/api/controller/dtos/UserListDto.java
+++ src/main/java/org/kush/share/api/controller/dtos/UserListDto.java
@@ -3,7 +3,7 @@ package org.kush.share.api.controller.dtos;
 import java.util.List;
 
 public record UserListDto(
-        String listId,
+        String id,
         String listName,
         String createdBy,
         List<ListItemDto> listItems
